c81e4a1268d3c28cceb889c803e1a9e918b69685
tahaproo <44582827+tahaproo@users.noreply.github.com>
2018-11-18 14:41:41 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -12416,4 +12416,31 @@ message.member.addRole(message.guild.roles.find("name", "100"));
 });
 
 
+client.on('message', msg => {
+     if(msg.content === 'هلا')
+                 setTimeout(function() {
+   msg.reply('هلا حبي')
+                 }, 0)
+   });
+client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('discord.gg')){
+        message.reply (' ')
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', 'Muted')); 
+    const embed500 = new Discord.RichEmbed()
+      .setTitle("❌ | تمت معاقبتك")
+            .addField(`** لقد قمت بمخالفة قوانين السيرفر من خلال نشر سيرفرات اخرى  **` , `**ملاحظة  : إن كآن هذآ الميوت عن طريق الخطأ تكلم مع الادآرة**`)
+      .addField(`by`,`shyboy_05`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL) 
+        .setFooter(`${message.guild.name} Server`)
+     message.channel.send(embed500) 
+    
+        
+    }
+    }
+})
 client.login(process.env.BOT_TOKEN);
